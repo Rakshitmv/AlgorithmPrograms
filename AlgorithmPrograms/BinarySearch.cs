f0574d18b6261b1eb22e831d2d63e246b2e41bc8
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-15 20:24:36 +0530

    [Rakshit] Add: Binary Search

---
+++ AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public void BinarySearchIterative(string userInput)
+        {
+            string[] strInputArray = { "My", "name", "is", "rakshit", "vaghasia" };
+            int min = 0, mid = 0, result = -1;
+            int max = strInputArray.Length - 1;
+
+            Console.WriteLine("Array values are:\n");
+            for (int i = 0; i <= max; i++)
+            {
+                Console.WriteLine(strInputArray[i]);
+            }
+            
+
+            while (min <= max)
+            {
+                mid = min + (min - 1) / 2;
+
+                if (userInput.Equals(strInputArray[mid]))
+                {
+                    result = 0;
+                }
+
+                if (result == 0)
+                {
+                    Console.WriteLine("match found");
+                }
+
+                int val = Convert.ToInt32(userInput.CompareTo(strInputArray[mid]));
+
+                if (val == 1)
+                {
+                    min = mid + 1;
+                    result = 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                    result = 1;
+                }
+            }
+
+            if (result == -1)
+            {
+                Console.WriteLine("String not found");
+            }
+            else if (result == 1)
+            {
+                Console.WriteLine("String found");
+            }
+
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs\n" + "Enter 1: Binary Search\n" + "Enter 2: Exit\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearch binarySearch = new BinarySearch();
+                    System.Console.WriteLine("Enter string value to search:");
+                    string userInput = Console.ReadLine();
+                    System.Console.WriteLine("User input is :" + userInput);
+                    binarySearch.BinarySearchIterative(userInput);
+                    break;
+                case 2:
+                    Console.WriteLine("Exit");
+                    break;
+                default:
+                    Console.WriteLine("enter valid option");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
